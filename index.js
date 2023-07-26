36d958b2daa1adf7ecbb850b20a82de388198944
Melisa <melisahrnn@gmail.com>
2023-07-26 13:38:31 -0400

    New Functions added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const strengther = document.querySelector('.meter');
 const passwordInput = document.querySelector('input[type="text"]');
 const passwordCheck = document.querySelector('.password-check');
 
+
+//Create password input eventlistener
 passwordInput.addEventListener('input', updateStrengther)
 function updateStrengther(){
     const assessments = calculatePasswordStrength(passwordInput.value);
@@ -17,5 +19,66 @@ function updateStrengther(){
         pwdCheckElem.innerHTML = assessment.pwdCheck;
         passwordCheck.appendChild(pwdCheckElem);
     });
-    strengther.computedStyleMap.setProperty('--meter-amount', strength);
+    strengther.style.setProperty("--meter-amount", strength);
+}
+
+//Function for calculating the password strength
+function calculatePasswordStrength(password){
+    const assessment = [];
+    assessment.push(lengthAssessment(password));
+    assessment.push(lowercaseAssessment(password));
+    assessment.push(uppercaseAssessment(password));
+    return assessment;
+}
+//Length Assessment function
+function lengthAssessment(password){
+    const length = password.length;
+    //console.log(length);
+    if(length <= 5){
+        return{
+            pwdCheck: "Password is too short",
+            strengthLost: 40,
+        };
+    }
+    if(length <= 10){
+        return{
+            pwdCheck:"Password could be longer",
+            strengthLost:15,
+        };
+    }
+}
+//Lowercase character assessment function
+function lowercaseAssessment(password){
+    const charMatch = password.match(/[a-z]/g) || [];
+    //console.log(charMatch);
+
+    if(charMatch.length === 0){
+        return{
+            pwdCheck: "Password has no lowercase character",
+            strengthLost: 20,
+        };
+    }
+    if(charMatch.length <= 2){
+        return{
+            pwdCheck:"Password needs more lowercase character",
+            strengthLost: 5,
+        };
+    }
+}
+//Uppercase character assessment function
+function uppercaseAssessment(password){
+    const charMatch = password.match(/[A-Z]/g) || [];
+
+    if(charMatch.length === 0){
+        return{
+            pwdCheck: "Password has no uppercase character",
+            strengthLost: 20,
+        };
+    }
+    if(charMatch.length <= 2){
+        return{
+            pwdCheck:"Password needs more uppercase character",
+            strengthLost: 5,
+        };
+    }
 }
